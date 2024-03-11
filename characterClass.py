7ed054a9d7cda59faaaca53e431e3792b70e61fd
JMC2246 <159717961+JMC2246@users.noreply.github.com>
2024-03-11 00:41:45 -0400

    Update characterClass.py
    Updated comments and cleaned up extra code

--- characterClass.py
+++ characterClass.py
@@ -1,3 +1,11 @@
+'''
+
+Represents a character entity in the game, which has stats such as health, attack, and defense
+Also contains methods such as add_to_inventory and print character info that allows for interaction
+With character aspects such as the inventory and stats
+
+'''
+
 class Character:
     def __init__(self, name, health, attack, defense):
         self.name = name
@@ -8,23 +16,18 @@ class Character:
         self.defense = defense
         self.inventory = []
 
-#Currently the inventory is just a list of strings, we can change that if needed, and for now the only "functional" items are the shield and sword increasing defense and attack respectively
-
-    #Adds an item to inventory
+    # Adds an item to inventory
     def add_to_inventory(self, item):
         self.inventory.append(item)
         self.check_inventory()
 
-    #Takes an item from inventory, prints are commented out because I was just using them to double check
+    # Takes an item from inventory
     def remove_from_inventory(self, item):
         if item in self.inventory:
             self.inventory.remove(item)
             self.check_inventory()
-            #print(f"{item} removed from inventory.")
-        #else:
-            #print(f"{item} not found in inventory.")
 
-#Allows for the characters stats to be changed based on items in their inventory, it is called after each add, remove, and print inventory to keep things updated
+    # Allows for the characters stats to be changed based on items in their inventory
     def check_inventory(self):
         if "Sword" in self.inventory:
             self.attack = self.base_attack + 5
@@ -36,7 +39,7 @@ class Character:
         else:
             self.defense = self.base_defense
 
-#Allows for the character information to be printed at any time
+    # Prints character information
     def print_character_info(self):
         self.check_inventory()
         print("Character Name:", self.name)
@@ -45,6 +48,7 @@ class Character:
         print("Character Defense:", self.defense)
         print("Character Inventory:", self.inventory)
 
+    # Causes damage from one character to another, based on their attack and defense stats.
     def take_damage(self, attacker):
         damage = max(0, attacker.attack - self.defense)
         self.health -= damage
@@ -54,34 +58,10 @@ class Character:
         else:
             print(f'{self.name} has {self.health} health left.')
 
-# Allows for input to be taken from the user, for this instance I only added i to view character info and q to quit the program. It is also commented out because it was rendered obsolete from the startMenu code
-#def handle_input():
-    #user_input = input("Press 'i' to view character info, 'q' to quit: ")
-    #if user_input.lower() == "i":
-        #player.print_character_info()
-    #elif user_input.lower() == "q":
-        #print("Exiting program.")
-        #quit()  # Exit the program
-    #else:
-        #print("Invalid input. Please try again.")
 
-# Example character
-#if __name__ == "__main__":
-    # Create a character with initial attributes and a name
-    #player = Character(name="TestCharacter", health=100, attack=5, defense=10)
-
-    # Add items to the inventory, tests remove on Arrows
-    #player.add_to_inventory("Potion")
-    #player.add_to_inventory("Sword")
-    #player.add_to_inventory("Shield")
-    #player.add_to_inventory("Arrows")
-    #player.remove_from_inventory("Arrows")
-
-    #Continuously checks for user input
-    #while True:
-        #handle_input()
 
 class CharacterDeathException(BaseException):
+
     """
     Description: Exception for when a character dies handled in game.py raised in character.py
     """
