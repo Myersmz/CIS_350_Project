0b769a1f5683bccc1d3166eeac5f907f7c858349
Redemxx <lost_diamond@charter.net>
2024-04-15 23:05:46 -0400

    Added powershell script for running all tests and making the html report. Renamed encounter_test.

---
+++ tests.ps1
@@ -0,0 +1,7 @@
+coverage run --branch --append -m unittest -v statistic_test
+coverage run --branch --append -m unittest -v character_test
+coverage run --branch --append -m unittest -v encounter_test
+coverage run --branch --append -m unittest -v floor_test
+coverage run --branch --append -m unittest -v item_test
+coverage run --branch --append -m unittest -v room_test
+coverage html
